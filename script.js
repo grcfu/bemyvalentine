1b088b50f723103963f369a6695e856ad1540582
grcfu <gracefu@wustl.edu>
2026-02-12 14:01:38 -0600

    partner profile part styled

--- script.js
+++ script.js
@@ -18,22 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     updateTimer();
     setInterval(updateTimer, 1000 * 60 * 60);
 
-    const accordionHeaders = document.querySelectorAll('.accordion-header');
-    
-    accordionHeaders.forEach(header => {
-        header.addEventListener('click', () => {
-            const item = header.parentElement;
-            
-            item.classList.toggle('active');
-
-            accordionHeaders.forEach(otherHeader => {
-                if (otherHeader !== header) {
-                    otherHeader.parentElement.classList.remove('active');
-                }
-            });
-        });
-    });
-
     const memoryImages = document.querySelectorAll('.memory-cell img');
 
     memoryImages.forEach(img => {
